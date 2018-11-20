c0683b828385142931793c286b18a8194bb85e62
radoslaw bobrowicz <rbobro@gmail.com>
2018-11-19 20:56:34 -0800

    Step 0 completed
    Using Haskeline library for line editing and history.
    History is saved in XDG_DATA/mal/history.

--- hal/src/step0_repl.hs
+++ hal/src/step0_repl.hs
@@ -1,5 +1,38 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Main where
+
+import System.Console.Haskeline
+import System.Directory
+
+halRead :: String -> String
+halRead = id
+
+halEval :: String -> String
+halEval = id
+
+halPrint :: String -> String
+halPrint = id
+
+rep :: String -> String
+rep = halPrint . halEval . halRead
+
+
+haskelineSettings :: FilePath -> Settings IO
+haskelineSettings fp = defaultSettings
+  { historyFile = Just fp
+  }
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  dataDir <- getXdgDirectory XdgData "mal"
+  createDirectoryIfMissing True dataDir
+  let histFile = dataDir ++ "/history"
+  runInputT (haskelineSettings histFile) loop
+    where
+      loop :: InputT IO ()
+      loop = getInputLine "user> " >>= \case
+        Nothing -> pure ()
+        Just input -> do
+          outputStrLn . rep $ input
+          loop
